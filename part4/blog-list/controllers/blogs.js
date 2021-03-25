51a323e168219617d092456e42403e020896580c
Oleksandr Stepanenko <sashastepanenko2007@gmail.com>
2021-03-25 20:26:38 +0200

    refactor into separate modules

---
+++ part4/blog-list/controllers/blogs.js
@@ -0,0 +1,24 @@
+const Blog = require('../model/blog')
+const blogsRouter = require('express').Router()
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const body = request.body
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog.save().then((savedBlog) => {
+    response.json(savedBlog)
+  })
+})
+
+module.exports = blogsRouter
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
-const http = require('http')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
-const blogsRouter = express.Router()
+const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, {
@@ -18,35 +17,6 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {
   console.log('connected to MongoDB')
-})
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-blogsRouter.post('/', (request, response) => {
-  const body = request.body
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  blog.save().then((savedBlog) => {
-    response.json(savedBlog)
-  })
 })
 
 app.use(cors())
